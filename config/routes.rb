8759ef9140ff652fbe1acbb15fb4917f8396d733
igor <zawgorodskiy.2012@gmail.com>
2018-01-22 13:59:47 +0200

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'session/new'
+
   get 'users/new'
 
   get 'static_pages/home'
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
+
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 
---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe SessionController, type: :controller do
+
+  describe "GET #new" do
+    it "returns http success" do
+      
+    end
+  end
+
+end
